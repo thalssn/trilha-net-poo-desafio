83d789fbc9a6eafed630470f95a4fe10fb96c500
Vinicius Lima <thalissonvini@outlook.com>
2025-08-24 13:09:40 -0300

    update: Solicitado ao user digitar qual app a ser instalado

--- Program.cs
+++ Program.cs
@@ -6,7 +6,10 @@ Console.WriteLine("Smartphone Nokia");
 Smartphone nokia = new Nokia(numero: "8498765-4321", modelo: "N8", imei: "11223344", memoria: 128);
 nokia.Ligar();
 nokia.ReceberLigacao();
-nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\nQual aplicativo voce deseja instalar no Nokia?");
+string appNokia = Console.ReadLine();
+nokia.InstalarAplicativo(appNokia);
 
 Console.WriteLine("\n");
 
@@ -14,4 +17,7 @@ Console.WriteLine("Smartphone iPhone");
 Smartphone iphone = new Iphone(numero: "8594321-9876", modelo: "16 Pro", imei: "55667788", memoria: 256);
 iphone.Ligar();
 iphone.ReceberLigacao();
-iphone.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\nQual aplicativo voce deseja instalar no iPhone?");
+string appIphone = Console.ReadLine();
+iphone.InstalarAplicativo(appIphone);
